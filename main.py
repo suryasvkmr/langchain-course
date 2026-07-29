07136e04bc27aaacdf24d52e6097b23a760da7eb
suryasvkmr <suryasivakumar013@gmail.com>
2026-07-28 23:05:02 -0400

    final react search agent for jobs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
 from langchain.agents import create_agent
@@ -10,6 +12,17 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 from tavily import TavilyClient
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent's response"""
+
+    answer: str = Field(description="The answer from the agent")
+    sources: List[Source] = Field(default_factory=list, description="Sources used by the agent")
 
 tavily = TavilyClient()
 
@@ -28,7 +41,7 @@ def search(query: str) -> str:
 llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
 #llm = ChatOllama(model="gemma3:270m") DOES NOT WORK
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
